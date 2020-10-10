2703c5d94b59d97c659cade630818ec600470c92
viowla <55015538+viowla@users.noreply.github.com>
2020-10-10 17:36:52 +0600

    edit EurekaApplication.java

--- src/main/java/kz/iitu/eureka/EurekaApplication.java
+++ src/main/java/kz/iitu/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaApplication {
 
     public static void main(String[] args) {
