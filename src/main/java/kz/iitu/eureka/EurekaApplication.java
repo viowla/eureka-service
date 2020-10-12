3b125ed3ab1e40f32f0333dad33107978eea729d
viowla <55015538+viowla@users.noreply.github.com>
2020-10-12 09:38:52 +0600

    enableClient->enableServer

--- src/main/java/kz/iitu/eureka/EurekaApplication.java
+++ src/main/java/kz/iitu/eureka/EurekaApplication.java
@@ -2,10 +2,12 @@ package kz.iitu.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
--- src/main/java/kz/iitu/eureka/UserRole.java
+++
@@ -1,5 +0,0 @@
-package kz.iitu.eureka;
-
-public enum UserRole {
-    USER, ADMIN;
-}
